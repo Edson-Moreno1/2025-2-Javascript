f5eb437edfdd08590dc844516f36bdb61a015c27
Edson-Moreno1 <edson.morenoortega@gmail.com>
2025-05-22 18:57:16 -0600

    update 22-may

---
+++ student-manager/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let students = [];
+function addStudent(){
+    console.log("Agregar estudiante");
+    const name = document.getElementById("nameInput").value.trim();
+    const grade = parseFloat(document.getElementById("gradeInput").value);
+
+    return;
+
+
+}
